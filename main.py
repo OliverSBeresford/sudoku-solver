2d2eeef401e8b1d61012cdd5e65f4d188240b1bc
Oliver Beresford <118030320+OliverSBeresford@users.noreply.github.com>
2024-11-07 21:25:50 -0500

    Fixed some styling issues

--- main.py
+++ main.py
@@ -3,32 +3,37 @@ from sudoku import Sudoku
 import sys
 
 def main():
-  if len(sys.argv) < 2:
-      print("Using an example:\n")
-      input_file = "puzzles.txt"
-  else:
-    print("Using input:\n")
-    input_file = sys.argv[1]
-  input_file = open(input_file, "r")
-  last = (0, 0)
-  sudoku = input_file.readline().strip()
-  sudoku = Sudoku(np.array(
-    [np.array(list(map(int, sudoku[x:x + 9]))) for x in range(0, 81, 9)]))
-  print("Original sudoku:\n", sudoku.grid)
-  for y in range(8, -1, -1):
-    for x in range(8, -1, -1):
-      if not sudoku.grid[y][x]:
-        last = (y, x)
-        break
+    if len(sys.argv) < 2:
+        print("Using an example:\n")
+        input_file = "puzzles.txt"
     else:
-      continue
-    break
-  print("Is original sudoku solved? True/False\n", sudoku.isSolved())
-  print("Solved sudoku:\n", sudoku.solve(last))
-  print(
-    "Is solved sudoku actually solved? (If not the sudoku was unsolvable): True / False:\n",
-    sudoku.isSolved())
-  input_file.close()
+        print("Using input:\n")
+        input_file = sys.argv[1]
+    input_file = open(input_file, "r")
+    last = (0, 0)
+    sudoku = input_file.readline().strip()
+    sudoku = Sudoku(
+        np.array(
+            [np.array(list(map(int, sudoku[x:x + 9])))for x in range(0, 81, 9)]
+        )
+    )
+    print("Original sudoku:\n", sudoku.grid)
+    for y in range(8, -1, -1):
+        for x in range(8, -1, -1):
+            if sudoku.grid[y][x] == 0:
+                last = (y, x)
+                break
+        else:
+            continue
+        break
+
+    print("Is original sudoku solved? True/False\n", sudoku.isSolved())
+    print("Solved sudoku:\n", sudoku.solve(last))
+    print(
+        "Is solved sudoku actually solved? (If not the sudoku was unsolvable): True / False:\n",
+        sudoku.isSolved()
+    )
+    input_file.close()
 
 
 if __name__ == "__main__":
--- sudoku.py
+++ sudoku.py
@@ -3,75 +3,73 @@ import sys
 
 
 class Sudoku:
+    def __init__(self, grid):
+        self.grid = grid
 
-  def __init__(self, grid):
-    self.grid = grid
-
-  def around(self, y, x):
-    coordX, coordY = x / 3, y / 3
-    if coordX >= 2:
-      coordX = 7
-      if coordY >= 2:
-        coordY = 7
-      elif coordY >= 1:
-        coordY = 4
-      else:
-        coordY = 1
-    elif coordX >= 1:
-      coordX = 4
-      if coordY >= 2:
-        coordY = 7
-      elif coordY >= 1:
-        coordY = 4
-      else:
-        coordY = 1
-    else:
-      coordX = 1
-      if coordY >= 2:
-        coordY = 7
-      elif coordY >= 1:
-        coordY = 4
-      else:
-        coordY = 1
-    return np.array([
-      self.grid[r][c] for r in range(coordY - 1, coordY + 2)
-      for c in range(coordX - 1, coordX + 2)
-    ])
-
-  def check(self, y, x, num):
-    if np.count_nonzero(self.around(y, x) == num) > 1:
-      return False
-    if np.count_nonzero(self.grid[0:9, x] == num) > 1 or np.count_nonzero(
-        self.grid[y] == num) > 1:
-      return False
-    return True
-
-  def find(self, end):
-    for index, num in np.ndenumerate(self.grid):
-      if num == 0:
-        for i in range(1, 10):
-          self.grid[index] = i
-          if self.check(index[0], index[1], i):
-            if index == end:
-              return True
+    def around(self, y, x):
+        coordX, coordY = x / 3, y / 3
+        if coordX >= 2:
+            coordX = 7
+            if coordY >= 2:
+                coordY = 7
+            elif coordY >= 1:
+                coordY = 4
             else:
-              if not self.find(end):
-                continue
-              else:
-                break
+                coordY = 1
+        elif coordX >= 1:
+            coordX = 4
+            if coordY >= 2:
+                coordY = 7
+            elif coordY >= 1:
+                coordY = 4
+            else:
+                coordY = 1
         else:
-          self.grid[index] = 0
-          return False
+            coordX = 1
+            if coordY >= 2:
+                coordY = 7
+            elif coordY >= 1:
+                coordY = 4
+            else:
+                coordY = 1
+        return np.array([
+            self.grid[r][c] for r in range(coordY - 1, coordY + 2)
+            for c in range(coordX - 1, coordX + 2)
+        ])
+
+    def check(self, y, x, num):
+        if np.count_nonzero(self.around(y, x) == num) > 1:
+            return False
+        if np.count_nonzero(self.grid[0:9, x] == num) > 1 or np.count_nonzero(self.grid[y] == num) > 1:
+            return False
         return True
 
-  def solve(self, end):
-    self.find(end)
-    return self.grid
+    def find(self, end):
+        for index, num in np.ndenumerate(self.grid):
+            if num == 0:
+                for i in range(1, 10):
+                    self.grid[index] = i
+                    if self.check(index[0], index[1], i):
+                        if index == end:
+                            return True
+                        else:
+                            if not self.find(end):
+                                continue
+                            else:
+                                break
+                else:
+                    self.grid[index] = 0
+                    return False
+                return True
 
-  def isSolved(self):
-    for row in range(self.grid.shape[0]):
-      for col in range(self.grid.shape[1]):
-        num = self.grid[row, col]
-        if not num or not self.check(col, row, num):
-          return False
-    return True
+    def solve(self, end):
+        self.find(end)
+        return self.grid
+
+    def isSolved(self):
+        for row in range(self.grid.shape[0]):
+            for col in range(self.grid.shape[1]):
+                num = self.grid[row, col]
+                if not num or not self.check(col, row, num):
+                    return False
+        return True
